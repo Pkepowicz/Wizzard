a39fef7e24133fe18d658bc706100dfacd385ea0
Pkepowicz <paw.kepowicz@gmail.com>
2022-10-29 21:57:10 +0200

    Added damage struct

---
+++ Wizzard/Assets/Scripts/Damage.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Damage 
+{
+    public int damageAmmount;
+    public float knockBack;
+    public Vector3 origin;
+};
