49538bd779faec42f52ab97d394808bca43732a1
Pkepowicz <paw.kepowicz@gmail.com>
2022-10-30 16:59:44 +0100

    WORKING PATHFINDING!!!

---
+++ Wizzard/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+// Note this line, if it is left out, the script won't know that the class 'Path' exists and it will throw compiler errors
+// This line should always be present at the top of scripts which use pathfinding
+using Pathfinding;
+
+[HelpURL("http://arongranberg.com/astar/docs/class_partial1_1_1_astar_a_i.php")]
+public class EnemyAI : MonoBehaviour
+{
+    public Transform targetPosition;
+
+    private Seeker seeker;
+    private Rigidbody2D rb;
+
+    public Path path;
+
+    public float speed = 2;
+
+    public float maxVelocity = 0.8f;
+
+    public float nextWaypointDistance = 3;
+
+    private int currentWaypoint = 0;
+
+    public float repathRate = 0.5f;
+    private float lastRepath = float.NegativeInfinity;
+
+    public bool reachedEndOfPath;
+
+    public void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        // If you are writing a 2D game you should remove this line
+        // and use the alternative way to move sugggested further below.
+        rb = GetComponent<Rigidbody2D>();
+
+        // Start a new path to the targetPosition, call the the OnPathComplete function
+        // when the path has been calculated (which may take a few frames depending on the complexity)
+        seeker.StartPath(transform.position, targetPosition.position, OnPathComplete);
+    }
+
+    public void OnPathComplete(Path p)
+    {
+        Debug.Log("A path was calculated. Did it fail with an error? " + p.error);
+
+        if (!p.error)
+        {
+            path = p;
+            // Reset the waypoint counter so that we start to move towards the first point in the path
+            currentWaypoint = 0;
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        if (Time.time > lastRepath + repathRate && seeker.IsDone())
+        {
+            lastRepath = Time.time;
+
+            // Start a new path to the targetPosition, call the the OnPathComplete function
+            // when the path has been calculated (which may take a few frames depending on the complexity)
+            seeker.StartPath(transform.position, targetPosition.position, OnPathComplete);
+        }
+        if (path == null)
+        {
+            // We have no path to follow yet, so don't do anything
+            return;
+        }
+
+        // Check in a loop if we are close enough to the current waypoint to switch to the next one.
+        // We do this in a loop because many waypoints might be close to each other and we may reach
+        // several of them in the same frame.
+        reachedEndOfPath = false;
+        // The distance to the next waypoint in the path
+        float distanceToWaypoint;
+        while (true)
+        {
+            // If you want maximum performance you can check the squared distance instead to get rid of a
+            // square root calculation. But that is outside the scope of this tutorial.
+            distanceToWaypoint = Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]);
+            if (distanceToWaypoint < nextWaypointDistance)
+            {
+                // Check if there is another waypoint or if we have reached the end of the path
+                if (currentWaypoint + 1 < path.vectorPath.Count)
+                {
+                    currentWaypoint++;
+                }
+                else
+                {
+                    // Set a status variable to indicate that the agent has reached the end of the path.
+                    // You can use this to trigger some special code if your game requires that.
+                    reachedEndOfPath = true;
+                    break;
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        // Slow down smoothly upon approaching the end of the path
+        // This value will smoothly go from 1 to 0 as the agent approaches the last waypoint in the path.
+        var speedFactor = reachedEndOfPath ? Mathf.Sqrt(distanceToWaypoint / nextWaypointDistance) : 1f;
+
+        // Direction to the next waypoint
+        // Normalize it so that it has a length of 1 world unit
+        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+
+        rb.AddForce(dir * speed * Time.deltaTime * speedFactor);
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxVelocity);
+        // If you are writing a 2D game you should remove the CharacterController code above and instead move the transform directly by uncommenting the next line
+        // transform.position += velocity * Time.deltaTime;
+    }
+}
