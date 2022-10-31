2634adee36e929727e1f596985dc47e945363ff4
ddziechciarz <alawon12@gmail.com>
2022-10-31 17:37:54 +0100

    commented code
    added comments to code and tidied it up

--- Wizzard/Assets/Scripts/WeaponScripts/Fireball.cs
+++ Wizzard/Assets/Scripts/WeaponScripts/Fireball.cs
@@ -14,7 +14,7 @@ public class Fireball : Projectile
     
     // variables for explosion
     public int explosionDamage = 3;
-    public float explosionRadius = 1f; // 0.3 gets multiplayed by radius, 1 is default value
+    public float explosionRadius = 1f; // basic radius gets multiplied by this value 
     public float explosionKnockbackForce = 2f;
 
     
@@ -38,6 +38,7 @@ public class Fireball : Projectile
         }
     }
 
+    // what to do with projectile when it's life ends
     protected override void OnProjectileFinish()
     {
         if (explodeAtDeath is true)
@@ -47,6 +48,8 @@ public class Fireball : Projectile
         Destroy(gameObject);
     }
 
+    
+    // what to do with projectile when it hits the wall
     protected override void OnProjectileWallHit()
     {
         if (explodeAtDeath is true)
@@ -61,10 +64,8 @@ public class Fireball : Projectile
     {
         Debug.Log("Exploding");
         GameObject currentExplosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
-        FireballExplosion explosionScript = currentExplosion.GetComponent<FireballExplosion>();
         
-        explosionScript.PassParameters(explosionDamage, explosionRadius, explosionKnockbackForce);
-        
-        Debug.Log("Destroying fireball");
+        // pass parameters about explosion to newly created explosion object
+        currentExplosion.GetComponent<FireballExplosion>().PassParameters(explosionDamage, explosionRadius, explosionKnockbackForce);
     }
 }
--- Wizzard/Assets/Scripts/WeaponScripts/FireballExplosion.cs
+++ Wizzard/Assets/Scripts/WeaponScripts/FireballExplosion.cs
@@ -4,48 +4,30 @@ using UnityEngine;
 
 public class FireballExplosion : Collidable
 {
+    // animation clip of explosion
     public AnimationClip animClip;
     
-    public bool explodeNow = false;
-
-    //private CircleCollider2D explosionCollider;
-
     private int explosionDamage;
     private float knockback;
+    
     void Start()
     {
-        //explosionCollider = GetComponent<CircleCollider2D>();
-        //Debug.Log(explosionCollider.radius);
         // destroy this gameObject at the end of its animation
         Destroy(gameObject, animClip.length);
-        //Debug.Log(animClip.length);
-        
-        
     }
-
     
-    /*void Update()
-    {
-        if (explodeNow)
-        {
-            base.Update();
-            explodeNow = false;
-            Debug.Log("Dealt Damage");
-        }
-        
-    }*/
 
+    // change parameters to one passed from fireball, so all variables are adjustable in fireball
     public void PassParameters(int damage, float rad, float knockbackForce)
     {
         explosionDamage = damage;
         knockback = knockbackForce;
-        //Debug.Log("Passed parameters");
-         
-         gameObject.transform.localScale = new Vector3(1.5f * rad, 1.5f * rad, 1);
+        gameObject.transform.localScale = new Vector3(1.5f * rad, 1.5f * rad, 1);
     }
 
     protected override void OnCollide(Collider2D coll)
     {
+        // send damage to enemy if hit by explosion
         if (coll.CompareTag("Enemy"))
         {
             Damage dmg = new Damage()
@@ -56,7 +38,6 @@ public class FireballExplosion : Collidable
             };
             
             coll.SendMessage("ReceiveDamage", dmg);
-            Debug.Log("Sending Damage");
         }
     }
 }
