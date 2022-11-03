fa7a4cfd635e2b0b74536bcd08cec1aace900a09
Pkepowicz <paw.kepowicz@gmail.com>
2022-11-03 20:39:37 +0100

    Added hand to enemies

--- Wizzard/Assets/Scripts/EnemyScripts/EnemyAI.cs
+++ Wizzard/Assets/Scripts/EnemyScripts/EnemyAI.cs
@@ -12,6 +12,8 @@ public class EnemyAI : Fighter
 
     public Transform targetPosition;
 
+    [SerializeField] Transform hand;
+
     private Seeker seeker;
 
     public Path path;
@@ -32,7 +34,6 @@ public class EnemyAI : Fighter
         base.Start();
         seeker = gameObject.GetComponent<Seeker>();
         targetPosition = GameObject.FindWithTag("Player").GetComponent<Transform>();
-        healthBar = gameObject.GetComponent<HealthBar>();
 
         // Start a new path to the targetPosition, call the the OnPathComplete function
         // when the path has been calculated (which may take a few frames depending on the complexity)
@@ -107,6 +108,12 @@ public class EnemyAI : Fighter
         // Direction to the next waypoint
         // Normalize it so that it has a length of 1 world unit
         Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+
+        // Rotate hand into walk/player direction
+        if (hand != null)
+        {
+            hand.transform.up = Vector2.Lerp(transform.up, targetPosition.transform.position - transform.position, 1.0f);
+        }
 
         // Move only if target is outside of desired range
         if (Vector2.Distance(transform.position, targetPosition.position) >= stopRadius)
