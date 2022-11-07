579319c177692319642ad20c6a16a0987e834433
Pkepowicz <paw.kepowicz@gmail.com>
2022-11-07 22:50:32 +0100

    spear wip

---
+++ Wizzard/Assets/Scripts/EnemyScripts/Spear.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spear : MonoBehaviour
+{
+    public EnemyAI enemyAi;
+    public Animator anim;
+    public float attackSpeed;
+    public float attackRange;
+    public float chargeForce;
+    private float lastAttack = float.NegativeInfinity;
+
+    private void Start()
+    {
+        enemyAi = GetComponentInParent<EnemyAI>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if ((enemyAi.transform.position - enemyAi.targetPosition.transform.position).magnitude <= attackRange && (Time.time - lastAttack) > attackSpeed)
+        {
+            enemyAi.canMove = false;
+            anim.SetTrigger("Draw");
+            lastAttack = Time.time;
+        }
+    }
+
+    public void addForce()
+    {
+        enemyAi.rb.AddForce((enemyAi.transform.position - enemyAi.targetPosition.transform.position).normalized * chargeForce);
+        enemyAi.canMove = true;
+    }
+}
