8a3bb97162efe4ffb9bd91f43d1164829d51f3db
dr3du4 <91844190+dr3du4@users.noreply.github.com>
2023-07-30 22:58:51 +0200

    Add better versuion od Destructable

--- Wizzard/Assets/Scripts/Destructable.cs
+++ Wizzard/Assets/Scripts/Destructable.cs
@@ -12,44 +12,35 @@ public class Destructable : Collidable
     public SpriteRenderer spriteRenderer;
     public GameObject gameObject;
     public int MaxHP;
-    public int HP;
-    void Start()
+    public float  HP;
     
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-
-       
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-            ChangeSprite(HP,MaxHP);
-            DestroyObject(HP);
-    }
-
-    public void ChangeSprite(int HP, int maxHP)
-    {
-        if (HP <= 0.9 * maxHP && HP > 0.7 * maxHP)
-        {
-            spriteRenderer.sprite = statusList[0];
+    
+    public void TakeDamage(float dmg){
+        HP =- dmg;
+        ChangeSprite();
+        if(HP <= 0){
+            DestroyThis();
         }
-        else if(HP <= 0.7 * maxHP && HP > 0.4 * maxHP)
-        {
-            spriteRenderer.sprite = statusList[1];
-        }   
-        else if(HP <= 0.4 * maxHP )
-        {
-            spriteRenderer.sprite = statusList[2];
-        }   
     }
 
-    public void DestroyObject(int HP)
-    {
-        if(HP<=0)
-            gameObject.SetActive(false);
+    public void ChangeSprite(){
+        switch(HP){
+            case  1 when HP< 0.9 * MaxHP:
+                spriteRenderer.sprite = statusList[0];
+                break;
+            case 2 when HP < 0.5 * MaxHP:
+                spriteRenderer.sprite = statusList[1];
+                break;
+            case 3 when HP < 0.3 * MaxHP:
+                spriteRenderer.sprite = statusList[2];
+                break;
+        }
     }
-    
+
+    private void DestroyThis(){
+        
+        Destroy(gameObject);
+    }
     
     
     
